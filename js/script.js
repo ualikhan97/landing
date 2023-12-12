10b9e474cc1559388089007109186732edbce7d2
ualikhan97 <supercilium97@gmail.com>
2023-12-12 14:33:36 +0600

    qwe

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+const swiper = new Swiper(".swiper", {
+   // Optional parameters
+   direction: "horizontal",
+   loop: true,
+
+   // If we need pagination
+   pagination: {
+      el: ".swiper-pagination",
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+
+   // And if we need scrollbar
+   scrollbar: {
+      el: ".swiper-scrollbar",
+   },
+});
+
 let burgerElement = document.querySelector(".burger");
 
 burgerElement.addEventListener("click", function () {
@@ -5,3 +27,22 @@ burgerElement.addEventListener("click", function () {
    document.querySelector(".header__shell").classList.toggle("active");
    document.body.classList.toggle("lock");
 });
+
+function addClassOnResize() {
+   let elements = document.querySelectorAll(".organisation__block");
+
+   let fifthElement = elements[4];
+   let sixthElement = elements[5];
+
+   let windowWidth = window.innerWidth;
+   if (windowWidth <= 400) {
+      fifthElement.classList.add("title");
+      sixthElement.classList.add("title");
+   } else {
+      fifthElement.classList.remove("title");
+      sixthElement.classList.remove("title");
+   }
+}
+
+document.addEventListener("DOMContentLoaded", addClassOnResize);
+window.addEventListener("resize", addClassOnResize);
